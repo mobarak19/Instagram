13924e5dba0b076f2c9377b2641d458077492482
Mobarak19 <mobarakhossen2012@gmail.com>
2022-07-05 11:42:51 +0600

    tab bar added

---
+++ Instagram/Controllers/Others/PublishPostVC.swift
@@ -0,0 +1,18 @@
+//
+//  PublishPostVC.swift
+//  Instagram
+//
+//  Created by Genusys Inc on 7/5/22.
+//
+
+import UIKit
+
+class PublishPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
