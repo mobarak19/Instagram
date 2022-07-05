0439ae1002a42173f3be7431390f2a2389f3f273
Mobarak19 <mobarakhossen2012@gmail.com>
2022-07-05 10:46:14 +0600

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Genusys Inc on 7/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
